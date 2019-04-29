9bc47392c9eed6adb28c36ace29eaa619f888577
Dalma Boros <dalma.boros@gmail.com>
2019-04-29 09:19:03 -0500

    Namespace routes as :api and :v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :shops
+      resources :coffees
+      
+      resources :flavors
+      resources :varieties
+      resources :processing_methods
+      resources :origins
+    end
+  end
 end
