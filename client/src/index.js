f6eb31a140b96acbaf1f13c86155c57479d9ca20
Dalma Boros <dalma.boros@gmail.com>
2019-04-30 08:02:03 -0500

    Add Navbar to Router in src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,66 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import ShopsContainer from './components/ShopsContainer';
+
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+/* Add the navbar component */
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set "exact" so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/shops"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Shops</NavLink>
+    <NavLink
+      to="/coffees"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Coffees</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
 
 ReactDOM.render((
   <Router>
-    <Route path="/" component={App} />
+    <React.Fragment>
+      <Navbar />
+      <Route exact path="/" component={App} />
+      <Route exact path="/shops" component={ShopsContainer} />
+    </React.Fragment>
   </Router>),
   document.getElementById('root')
 );
