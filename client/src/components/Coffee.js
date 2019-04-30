e7b9f661f09ba1fbbab803525b1151d5be4cdc8a
Dalma Boros <dalma.boros@gmail.com>
2019-04-30 07:18:16 -0500

    Reordered coffee attributes in Coffee component

--- client/src/components/Coffee.js
+++ client/src/components/Coffee.js
@@ -3,20 +3,10 @@ import React from 'react';
 const Coffee = ({coffee}) =>
     <div className="single-coffee" key={coffee.id}>
       <h4>{coffee.name}</h4>
-      <b>Description</b>: {coffee.description}<br/>
       <b>Roaster</b>: {coffee.roaster}<br/>
-      <b>Producer</b>: {coffee.producer}<br/>
-      <b>Washing Station</b>: {coffee.washing_station}<br/>
-      <b>Altitude</b>: {coffee.min_altitude}<br/>
-      <br/>
-      <b>Flavors</b>:
-      {coffee.flavors.map((flavor, index) => {
-        return (index ? ', ' : '') + " " + flavor.name
-      })}
-      <br/>
-      <b>Varieties</b>:
-      {coffee.varieties.map((variety, index) => {
-        return (index ? ', ' : '') + " " + variety.name
+      <b>Origin</b>:
+      {coffee.origins.map((origin, index) => {
+        return (index ? ', ' : '') + " " + origin.region
       })}
       <br/>
       <b>Processing</b>:
@@ -24,10 +14,22 @@ const Coffee = ({coffee}) =>
         return (index ? ', ' : '') + " " + processing_method.name
       })}
       <br/>
-      <b>Origin</b>:
-      {coffee.origins.map((origin, index) => {
-        return (index ? ', ' : '') + " " + origin.region
+      <b>Flavors</b>:
+      {coffee.flavors.map((flavor, index) => {
+        return (index ? ', ' : '') + " " + flavor.name
       })}
+      <br/>
+      <br/>
+
+      <b>Description</b>: {coffee.description}<br/>
+      <b>Producer</b>: {coffee.producer}<br/>
+      <b>Washing Station</b>: {coffee.washing_station}<br/>
+      <b>Altitude</b>: {coffee.min_altitude}<br/>
+      <b>Varieties</b>:
+      {coffee.varieties.map((variety, index) => {
+        return (index ? ', ' : '') + " " + variety.name
+      })}
+      <br/>
 
     </div>
 
